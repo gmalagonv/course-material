c38b709fa70381ec939ae61d1277fb3a30433045
gerardo <gemalvalk@gmail.com>
2014-09-23 20:01:44 +0200

    solution 100

---
+++ exercices/100/solution.py
@@ -0,0 +1,10 @@
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+for i in station:
+    a = station[i]
+    print(i, a)
