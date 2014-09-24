037291efc66db8f0ff2d6e1290540311adce6b82
gerardo <gemalvalk@gmail.com>
2014-09-24 20:10:11 +0200

    solut 200

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -1,15 +1,14 @@
 def is_prime(a):
     if a == 2 or a == 3:
-        print("True")
+        return True
     else:
         if a > 1:
             b = 2
             while b < a:
                 if a % b == 0:
-                    print("False")
-                    break
+                    return False
                 b = b + 1
             else:
-                print("True")
+                return True
         else:
-            print("False")
+            return False
