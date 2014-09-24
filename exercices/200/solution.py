13d7df7ab8d7a0af2526c8c1264c0af06f20dae8
gerardo <gemalvalk@gmail.com>
2014-09-24 15:26:50 +0200

    solut 200

---
+++ exercices/200/solution.py
@@ -0,0 +1,14 @@
+def is_prime(a):
+    if a == 2:
+        return True         
+    else:
+        if a > 1:
+            for i in range(3,a):
+                if a % i == 0:
+                    return False                    
+                else:
+                   return True
+        else:
+            print("input error")     
+        
+        
