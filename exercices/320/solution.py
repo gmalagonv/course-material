b39d81eb17d4b1c33630d9ad68b8d10a42615a2c
gerardo <gemalvalk@gmail.com>
2014-09-25 22:31:01 +0200

    solut 320

--- exercices/320/solution.py
+++ exercices/320/solution.py
@@ -1,25 +1,15 @@
-al=("abcdefghijklmnopqrstuvwxyz")
+al = ("abcdefghijklmnopqrstuvwxyz")
 al2 = al.upper()
 al3 = al+al2
-dic={}
+dic = {}
 for i in al3:
-    dic[i]=0
-print(dic)
-f = open('words2', 'r')
+    dic[i] = 0
+f = open('words', 'r')
 for line in f:
     for h in line:
-        if h in dic:            
-            dic[h]=dic[h]+1
-print(dic)
-        
-        
-    
-    
-#f = open('words', 'r')
-#for line in f:
- #   for h in line:
-  #      if h in al3:
-            
-   #             c = c + 1
-    #print(k,c)     
-            
+        if h in dic:
+            dic[h] = dic[h] + 1
+for p in dic:
+    if dic[p] != 0:
+        a = dic[p]
+        print(p + ":", a)
